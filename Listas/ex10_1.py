80fb219e2a981146a9fe8329f730a864c6ec98a0
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2023-04-12 21:48:02 -0300

    Exercicio 10.1 - Listas

---
+++ Listas/ex10_1.py
@@ -0,0 +1,16 @@
+"""
+Capituo 10 - listas
+Exercicio 10.1
+Escreva uma função chmada nested_sum que receba uma lista de listas
+de numero inteiros e adicione os elementos de todas as listas aninhadas.
+"""
+def nested_sum(lista):
+    soma = 0
+    for i in lista:
+        for j in i:
+            soma = soma + j
+    return soma
+
+
+t = [[1,2], [3], [4,5,6]]
+print(nested_sum(t))
