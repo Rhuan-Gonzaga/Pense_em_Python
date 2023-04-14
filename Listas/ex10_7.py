cb90fd86cfc6e46cc28d93878ef08be99aee5e7b
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2023-04-13 21:17:42 -0300

    Exercicio 10.7 Listas

---
+++ Listas/ex10_7.py
@@ -0,0 +1,13 @@
+"""
+Livro Pense em Python Capitulo 10 - listas
+Exercicio 10.7
+Escreva uma função chamada has_duplicates que tome uma lista e retorne True
+se houver algum elemento que apareça mais de uma vez. Ela não deve modificar a lista original
+"""
+def has_duplicates(lista):
+    return len(lista) != len(set(lista))
+
+
+lista = [7,8,9,2,9]
+print(has_duplicates(lista))
+
